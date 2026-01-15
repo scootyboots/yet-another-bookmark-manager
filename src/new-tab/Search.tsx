ac05f4778f39617d2ccd8a81072e50fda1e69a42
scootyboots <scott.coffrin@gmail.com>
2026-01-14 18:31:20 -0700

    highlight fuzzy matches

--- src/new-tab/Search.tsx
+++ src/new-tab/Search.tsx
@@ -2,6 +2,7 @@ import { useState, useMemo, useEffect, useRef, useCallback } from 'react'
 import { createPortal } from 'react-dom'
 import { BookmarkEntryProps } from './BookmarkEntry'
 import Prompt from './Prompt'
+import { search } from 'fast-fuzzy'
 
 export const LINK_TO_OPEN_SELECTOR = '[data-link-to-open]'
 export const IS_MATCH_SELECTOR = '[data-is-match]'
@@ -24,18 +25,29 @@ export default function Search({
   const [focusIndex, setFocusIndex] = useState(0)
   const [lastMatches, setLastMatches] = useState<Array<BookmarkEntryProps>>([])
 
-  const { matches, hasMatches } = useMemo(() => {
+  const { matches, hasMatches, fastFuzzy } = useMemo(() => {
     const inputRemovedSpaces = inputText.replace(/\s+/g, '')
+    const fuzzyText = search(inputRemovedSpaces, bookmarks, {
+      keySelector: (bk) => bk.text,
+      returnMatchData: true,
+    })
+    const fuzzyHref = search(inputRemovedSpaces, bookmarks, {
+      keySelector: (bk) => bk.href,
+      returnMatchData: true,
+    })
+    // console.log(fuzzyText, fuzzyHref)
     if (!inputRemovedSpaces) {
       setUrlToOpen('')
       setLastMatches([])
-      return { matches: [], hasMatches: false }
+      return { matches: [], hasMatches: false, fastFuzzy: [] }
     }
     const matches = findFuzzyMatches(inputRemovedSpaces, bookmarks)
-    const hasMatches = Array.isArray(matches) && matches.length > 0
-    if (hasMatches) setLastMatches(matches)
+    // const hasMatches = Array.isArray(matches) && matches.length > 0
+    const hasMatches = Array.isArray(fuzzyText) && fuzzyText.length > 0
+    const durr = fuzzyText as unknown as typeof matches
+    if (hasMatches) setLastMatches(durr)
     setUrlToOpen(matches?.[0]?.href ?? '')
-    return { matches, hasMatches }
+    return { matches, hasMatches, fastFuzzy: fuzzyText ?? [] }
   }, [inputText])
 
   const shakeX = useMemo(() => {
@@ -48,8 +60,9 @@ export default function Search({
   }, [matches, focusIndex])
 
   const matchesToRender = useMemo(
-    () => (hasMatches ? matches : lastMatches),
-    [hasMatches, matches, lastMatches]
+    () => (hasMatches ? fastFuzzy ?? [] : []),
+    // () => (hasMatches ? matches : lastMatches),
+    [hasMatches, matches, lastMatches, fastFuzzy]
   )
 
   const inputRef = useRef<HTMLInputElement>(null)
@@ -57,7 +70,7 @@ export default function Search({
   const keydownHandler = useCallback(
     (event: KeyboardEvent) => {
       const { key, metaKey } = event
-      console.log(key, event)
+      // console.log(key, event)
       function preventDefaultIfOpen() {
         const searchElement =
           document.querySelector<HTMLDivElement>('[data-search-open]')
@@ -136,6 +149,28 @@ export default function Search({
       <div style={{ paddingBlockStart: '1rem', position: 'relative' }}>
         {matchesToRender.map((match, index) => {
           const moreThan18 = index + 1 > MAX_DISPLAYED_RESULTS
+          function splitAtMatch(toSplit: string) {
+            const beforeMatch = match.item.text.slice(match.match.index)
+            const start = match.item.text.slice(
+              match.match.index,
+              match.match.length
+            )
+            const remaining = match.item.text.slice(
+              match.match.index + match.match.length
+            )
+            const marked = <mark>{start}</mark>
+            return (
+              <>
+                {beforeMatch}
+                {marked}
+                {remaining}
+              </>
+            )
+            return beforeMatch + <mark>{start}</mark> + remaining
+            return `${beforeMatch} | ${start} | ${remaining}`
+          }
+          const itemSplit = match.item.text
+          const hrefSplit = match.item.href
           if (moreThan18) return null
           return (
             <div
@@ -149,9 +184,11 @@ export default function Search({
               data-is-match
               key={'matching-bookmark-' + index}
             >
-              <div className="Search-result-text">{match.text}</div>
+              <div className="Search-result-text">
+                <HighlightedMatch toMatch={match.item.text} input={inputText} />
+              </div>
               <div className="Search-result-divider"> : </div>
-              <div className="Search-result-link">{match.href}</div>
+              <div className="Search-result-link">{match.item.href}</div>
             </div>
           )
         })}
@@ -173,9 +210,9 @@ export default function Search({
                   // borderColor: 'var(--primary-weak)',
                 }}
               >
-                {matches.length > MAX_DISPLAYED_RESULTS
-                  ? `${MAX_DISPLAYED_RESULTS} / ${matches.length}`
-                  : `${matches.length}`}
+                {fastFuzzy.length > MAX_DISPLAYED_RESULTS
+                  ? `${MAX_DISPLAYED_RESULTS} / ${fastFuzzy.length}`
+                  : `${fastFuzzy.length}`}
               </div>
             </div>
           </div>
@@ -184,6 +221,70 @@ export default function Search({
     </Prompt>
   )
 }
+
+function HighlightedMatch({
+  toMatch,
+  input,
+}: {
+  toMatch: string
+  input: string
+}) {
+  const toMatchLetters = toMatch.split('')
+  // const inputLetters = input.split('')
+  // let firstMatchFound = false
+  // let firstMatchIndex = 0
+  // let lastMatchIndex = 0
+  // const consecutiveMatches = []
+  // for (let index = 0; index < toMatchLetters.length; index++) {
+  //   const matchLetter = toMatchLetters[index].toLowerCase()
+  //   let consecutiveMatch = 1
+  //   for (let i = 0; i < inputLetters.length; i++) {
+  //     const inputLetter = inputLetters[i].toLowerCase()
+  //     const nextMatchLetter = inputLetter[i + 1]?.toLowerCase()
+  //     const isMatchedLetter = inputLetter === matchLetter
+  //     const isNextMatchedLetter = nextMatchLetter === matchLetter
+  //     if (isMatchedLetter && !firstMatchFound) {
+  //       firstMatchFound = true
+  //       firstMatchIndex = index
+  //     }
+  //     if (isMatchedLetter) {
+  //       lastMatchIndex = index
+  //       if (isNextMatchedLetter) {
+  //         consecutiveMatch++
+  //       }
+  //     }
+  //     consecutiveMatches.push({
+  //       car: matchLetter,
+  //       index,
+  //       length: consecutiveMatch,
+  //     })
+  //     consecutiveMatch = 0
+  //   }
+  // }
+  const bestMatch = highlightedMatch({ match: toMatch, input })
+  console.log(bestMatch)
+  const beforeMatch = toMatchLetters.slice(0, bestMatch.labelIndex)
+  const matched = toMatchLetters.slice(
+    bestMatch.labelIndex,
+    bestMatch.labelIndex + bestMatch.length
+  )
+  const afterMatched = toMatchLetters.slice(
+    bestMatch.labelIndex + bestMatch.length
+  )
+  if (toMatchLetters.includes('Unicode Lookup')) {
+    // console.log('consecutive matches', consecutiveMatches)
+  }
+
+  return (
+    <>
+      {beforeMatch}
+      <mark>{matched}</mark>
+      {afterMatched}
+    </>
+  )
+}
+
+// https://github.com/microsoft/vscode/blob/main/src/vs/base/common/fuzzyScorer.ts
 
 function findFuzzyMatches(input: string, bookmarks: SearchProps['bookmarks']) {
   // const groupFlagPattern = /^g:\s*(\S+)\s*(.*)/gi
@@ -234,4 +335,93 @@ function findFuzzyMatches(input: string, bookmarks: SearchProps['bookmarks']) {
   } catch {
     return []
   }
+}
+
+function highlightedMatch({ match, input }: { match: string; input: string }) {
+  const bookmarkLabel = match.split('')
+  const searchInput = input.split('')
+
+  const bestMatches = []
+  const query = searchInput
+  const ignoreCharacters = /[\s;:\.\?\!\,'"\|]/
+  for (let queryIndex = 0; queryIndex < query.length; queryIndex++) {
+    const queryCar = query[queryIndex].toLowerCase()
+
+    let matchLength = 0
+    let bestLength = 0
+    let bestMatch = {
+      start: queryCar,
+      car: '',
+      labelCar: '',
+      internalLabelIndex: 0,
+      labelIndex: 0,
+      length: 0,
+    }
+
+    for (let labelIndex = 0; labelIndex < bookmarkLabel.length; labelIndex++) {
+      const labelCar = bookmarkLabel[labelIndex].toLowerCase()
+      // TODO: expand match logic
+      const isCurrentMatch = queryCar === labelCar
+
+      if (isCurrentMatch) {
+        matchLength = 1
+
+        for (
+          let nextLabelIndex = 1 + labelIndex;
+          nextLabelIndex < bookmarkLabel.length;
+          nextLabelIndex++
+        ) {
+          let nextQueryIndex = matchLength + queryIndex
+          const nextQueryCar = query[nextQueryIndex]?.toLowerCase()
+          const nextLabelCar = bookmarkLabel[nextLabelIndex]?.toLowerCase()
+
+          const isContinueCarLabel = ignoreCharacters.test(nextLabelCar)
+          const isContinueCarQuery = ignoreCharacters.test(nextQueryCar)
+
+          // if (isContinueCarLabel && !isContinueCarQuery) {
+          //   console.log('skipping: ' + nextLabelCar)
+          //   // matchLength++
+          //   continue;
+          // }
+
+          if (!nextQueryCar) {
+            if (matchLength > bestLength) {
+              bestLength = matchLength
+              bestMatch.car = queryCar
+              bestMatch.labelCar = nextLabelCar
+              bestMatch.internalLabelIndex = nextLabelIndex
+              bestMatch.labelIndex = nextLabelIndex + 1 - bestLength
+              bestMatch.length = bestLength
+            }
+            // matchLength = 0
+            break
+          }
+          // console.log(queryCar, nextQueryCar)
+
+          const isNextMatch = nextQueryCar === nextLabelCar
+          if (isNextMatch) {
+            matchLength++
+            nextQueryIndex++
+            // console.log(nextQueryCar, nextLabelCar)
+            if (matchLength > bestLength) {
+              bestLength = matchLength
+              bestMatch.car = queryCar
+              bestMatch.labelCar = nextLabelCar
+              bestMatch.internalLabelIndex = nextLabelIndex
+              bestMatch.labelIndex = nextLabelIndex + 1 - bestLength
+              bestMatch.length = bestLength
+            }
+          }
+        }
+      }
+    }
+    if (bestMatch.length) {
+      bestMatches.push(bestMatch)
+    }
+  }
+
+  const longestMatch = bestMatches.sort((a, b) => b.length - a.length)[0]
+  console.log(bestMatches)
+  console.log('Longest Match:\n', longestMatch)
+  return longestMatch
 }
