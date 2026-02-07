a7f0a2a56c484b26af188913c91c4ff974f35788
scootyboots <scott.coffrin@gmail.com>
2026-02-07 17:18:17 +0900

    focus trap on prompt

--- src/new-tab/Prompt.tsx
+++ src/new-tab/Prompt.tsx
@@ -1,16 +1,26 @@
-import { PropsWithChildren } from 'react'
+import { FocusTrap } from 'focus-trap-react'
+import { PropsWithChildren, useEffect } from 'react'
+import { useTrackFocus } from './useTrackFocus'
 
 export default function Prompt({
   isShown,
   className,
   children,
 }: { isShown: boolean; className?: string } & PropsWithChildren) {
+  const { focusPreviousElement } = useTrackFocus()
+  useEffect(() => {
+    return () => {
+      focusPreviousElement()
+    }
+  }, [])
   return (
     <>
       <div className="Prompt-background" />
-      <div className={'Prompt' + ' ' + className} data-prompt-open={isShown}>
-        <div className="Prompt-content">{children}</div>
-      </div>
+      <FocusTrap>
+        <div className={'Prompt' + ' ' + className} data-prompt-open={isShown}>
+          <div className="Prompt-content">{children}</div>
+        </div>
+      </FocusTrap>
     </>
   )
 }
